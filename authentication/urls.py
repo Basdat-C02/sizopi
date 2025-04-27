00c707dd6a6584a4cbc437b3d8f06318c989c3b6
arshqiii <marshq23@gmail.com>
2025-04-27 11:40:03 +0700

    feat: created pages for home, login and choose role for register

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import home_view, login_view, choose_role_view, register_view
 
 app_name = 'authentication'
 
 urlpatterns = [
-    
+    path('', home_view, name='home'),
+    path('auth/login/', login_view, name='login'),
+    path('auth/choose-role/', choose_role_view, name='choose_role'),
+    path('auth/register/', register_view, name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'main/index.html')
+
+def login_view(request):
+    return render(request, 'login/index.html')
+
+def choose_role_view(request):
+    return render(request, 'choose_role/index.html')
+
+def register_view(request):
+    return render(request, 'register/index.html')
