211e0b13703c053ff9f319698d731f7502b81ddf
arshqiii <marshq23@gmail.com>
2025-04-28 08:27:06 +0700

    created views for add satwa form

--- satwa/urls.py
+++ satwa/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from satwa.views import add_satwa_view
 
 app_name = 'satwa'
 
 urlpatterns = [
-    
+    path('satwa/add_satwa/', add_satwa_view, name='add_satwa'),
 ]
--- satwa/views.py
+++ satwa/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def add_satwa_view(request):
+    status_kesehatan = [
+        'Sehat',
+        'Sakit',
+        'Luka',
+        'Mati'
+    ]
+    
+    daftar_habitat = [
+        'Hutan',
+        'Sungai',
+        'Laut',
+        'Padang Rumput'
+    ]
+    
+    context = {
+        'status_kesehatan': status_kesehatan,
+        'daftar_habitat': daftar_habitat,
+    }
+
+    return render(request, 'add_satwa/index.html', context)
