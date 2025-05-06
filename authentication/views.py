6f07d389f84fa37ea06503e55bad0901b1debcb6
arshqiii <marshq23@gmail.com>
2025-05-06 10:33:26 +0700

    feat: added views for register

--- authentication/views.py
+++ authentication/views.py
@@ -1,29 +1,123 @@
-from django.shortcuts import render
+import uuid
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from authentication.services.auth_service import AuthService
+from authentication.utils.jwt_utils import generate_jwt
+from django.http import HttpResponse
 
 # Create your views here.
 def landing_view(request):
     return render(request, 'landing/index.html')
 
-def login_view(request):
-    return render(request, 'login/index.html')
-
 def choose_role_view(request):
     return render(request, 'choose_role/index.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = AuthService.get_user_by_username(username)
+        if not user:
+            messages.error(request, 'Username tidak ditemukan')
+            return redirect('authentication:login')
+        
+        if not AuthService.check_password(password, user['password']):
+            messages.error(request, 'Password salah')
+            return redirect('authentication:login')
+        
+        token = generate_jwt(username)
+        response = redirect('authentication:profile')
+        response.set_cookie('jwt', token, httponly=True, samesite='Strict')
+        return response
+        
+    return render(request, 'login/index.html')
+
 def register_pengunjung_view(request):
     if request.method == 'POST':
-        # Validasi dan logika buat akun
-        pass
-    return render(request, 'register_pengunjung/index.html')
+        data = {
+            "username": request.POST.get("username"),
+            "password": request.POST.get("password"),
+            "email": request.POST.get("email"),
+            "nama_depan": request.POST.get("nama_depan"),
+            "nama_tengah": request.POST.get("nama_tengah"),
+            "nama_belakang": request.POST.get("nama_belakang"),
+            "no_telepon": request.POST.get("no_telepon"),
+            "alamat": request.POST.get("alamat"),
+            "tgl_lahir": request.POST.get("tanggal_lahir"),
+            "role": "pengunjung",
+        }
+        
+        if request.POST.get("password") != request.POST.get("confirm_password"):
+            messages.error(request, "Konfirmasi password tidak cocok.")
+            return redirect("authentication:register_pengunjung") 
+        
+        AuthService.create_profile(data)
+        messages.success(request, "Registrasi berhasil. Silakan login.")
+        return redirect("authentication:login")
+
+    return render(request, 'register/pengunjung.html')
 
 def register_dokter_hewan_view(request):
     if request.method == 'POST':
-        # Validasi dan logika buat akun
-        pass
-    return render(request, 'register_dokter/index.html')
+        spesialisasi = request.POST.getlist("spesialisasi")
+        lainnya = request.POST.get("spesialisasi_lainnya")
+        if lainnya:
+            spesialisasi.append(lainnya)
+        data = {
+            "username": request.POST.get("username"),
+            "password": request.POST.get("password"),
+            "email": request.POST.get("email"),
+            "nama_depan": request.POST.get("nama_depan"),
+            "nama_tengah": request.POST.get("nama_tengah"),
+            "nama_belakang": request.POST.get("nama_belakang"),
+            "no_telepon": request.POST.get("no_telepon"),
+            "no_str": request.POST.get("no_str"),
+            "spesialisasi": spesialisasi,
+            "role": "dokter_hewan",
+        }
+        
+        if request.POST.get("password") != request.POST.get("confirm_password"):
+            messages.error(request, "Konfirmasi password tidak cocok.")
+            return redirect("authentication:register_pengunjung") 
+        
+        AuthService.create_profile(data)
+        messages.success(request, "Registrasi berhasil. Silakan login.")
+        return redirect("authentication:login")
+    
+    return render(request, 'register/dokter.html')
 
 def register_staff_view(request):
     if request.method == 'POST':
-        # Validasi dan logika buat akun
-        pass
-    return render(request, 'register_staff/index.html')
+        peran = request.POST.get("peran")
+        id_staf = str(uuid.uuid4())
+        role_map = {
+            "Penjaga Hewan": "penjaga_hewan",
+            "Staf Administrasi": "staf_admin",
+            "Pelatih Pertunjukan": "pelatih_hewan"
+        }
+        mapped_peran = role_map.get(peran)
+        data = {
+            "username": request.POST.get("username"),
+            "password": request.POST.get("password"),
+            "email": request.POST.get("email"),
+            "nama_depan": request.POST.get("nama_depan"),
+            "nama_tengah": request.POST.get("nama_tengah"),
+            "nama_belakang": request.POST.get("nama_belakang"),
+            "no_telepon": request.POST.get("no_telepon"),
+            "role": mapped_peran,
+            "id_staf": id_staf
+        }
+        
+        if request.POST.get("password") != request.POST.get("confirm_password"):
+            messages.error(request, "Konfirmasi password tidak cocok.")
+            return redirect("authentication:register_pengunjung") 
+        
+        AuthService.create_profile(data)
+        messages.success(request, "Registrasi berhasil. Silakan login.")
+        return redirect("authentication:login")
+
+    return render(request, 'register/staf.html')
+
+def profile_view(request):
+    pass
