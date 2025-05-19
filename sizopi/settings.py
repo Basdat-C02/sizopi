f2895682d9889b9877963a4898e60844af264df3
arshqiii <marshq23@gmail.com>
2025-05-19 13:20:13 +0700

    cleaned main branch

--- sizopi/settings.py
+++ sizopi/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'hiburan',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,10 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'kesehatan',
-    'satwa',
-    'habitat',
-    'adopsi',
 ]
 
 MIDDLEWARE = [
